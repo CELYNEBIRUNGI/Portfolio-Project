f50e761311ad4ce3c574e9a61107cfe217b10d06
CELYNEBIRUNGI <113887926+CELYNEBIRUNGI@users.noreply.github.com>
2023-04-18 08:57:42 +0300

    Add form Validation

--- js/main.js
+++ js/main.js
@@ -10,4 +10,24 @@ const handleClick = () => {
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('#hamburger').addEventListener('click', handleClick);
   document.querySelectorAll('.mobile-menu-links, .disabled-button').forEach((button) => button.addEventListener('click', handleClick));
+
+  const form = document.querySelector('form');
+  const submitButton = document.querySelector('.form-button');
+
+  form.addEventListener('submit', (event) => {
+    const emailInput = document.querySelector('.email-box');
+    const email = emailInput.value.trim();
+
+    if (email !== email.toLowerCase()) {
+      const errorMessage = document.createElement('p');
+      errorMessage.classList.add('error-message');
+      errorMessage.textContent = 'email address must be in lowercase.';
+      const existingErrorMessage = submitButton.nextElementSibling;
+      if (existingErrorMessage && existingErrorMessage.classList.contains('error-message')) {
+        existingErrorMessage.remove();
+      }
+      submitButton.insertAdjacentElement('afterend', errorMessage);
+      event.preventDefault();
+    }
+  });
 });
