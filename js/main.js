967c8800006a78b6bf41b18db63696e4b66e1878
CELYNEBIRUNGI <113887926+CELYNEBIRUNGI@users.noreply.github.com>
2023-04-18 18:30:37 +0300

    update javascript/css files

--- js/main.js
+++ js/main.js
@@ -10,56 +10,51 @@ const handleClick = () => {
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('#hamburger').addEventListener('click', handleClick);
   document.querySelectorAll('.mobile-menu-links, .disabled-button').forEach((button) => button.addEventListener('click', handleClick));
+});
 
-  const form = document.querySelector('#form');
-  const submitButton = document.querySelector('.form-button');
+const form = document.querySelector('#form');
+const submitButton = document.querySelector('.form-button');
 
-  form.addEventListener('submit', (event) => {
-    const emailInput = document.querySelector('.email-box');
-    const email = emailInput.value.trim();
-
-    if (email !== email.toLowerCase()) {
-      const errorMessage = document.createElement('p');
-      errorMessage.classList.add('error-message');
-      errorMessage.textContent = 'Email address must be in lowercase.';
-      const existingErrorMessage = submitButton.nextElementSibling;
-      if (existingErrorMessage && existingErrorMessage.classList.contains('error-message')) {
-        existingErrorMessage.remove();
-      }
-      submitButton.insertAdjacentElement('afterend', errorMessage);
-      event.preventDefault();
+form.addEventListener('submit', (event) => {
+  const emailInput = document.querySelector('.email-box');
+  const email = emailInput.value.trim();
+  if (email !== email.toLowerCase()) {
+    const errorMessage = document.createElement('p');
+    errorMessage.classList.add('error-message');
+    errorMessage.textContent = 'Email address must be in lowercase.';
+    const existingErrorMessage = submitButton.nextElementSibling;
+    if (existingErrorMessage && existingErrorMessage.classList.contains('error-message')) {
+      existingErrorMessage.remove();
     }
+    submitButton.insertAdjacentElement('afterend', errorMessage);
+    event.preventDefault();
+  }
 
-     window.onload = () => {
-      if (localStorage) {
-        const localStorageItem = localStorage.getItem('formdata');
-        const formtoken = JSON.parse(localStorageItem);
-        document.getElementById('Name').value = formtoken.formname;
-        document.getElementById('email').value = formtoken.formemail;
-        document.getElementById('form').value = formtoken.formtext;
-      }
+  window.onload = () => {
+    if (localStorage) {
+      const localStorageItem = localStorage.getItem('formdata');
+      const formtoken = JSON.parse(localStorageItem);
+      document.getElementById('name').value = formtoken.formname;
+      document.getElementById('emailfield').value = formtoken.formemail;
+      document.getElementById('textfield').value = formtoken.formtext;
+    }
+  };
+  window.addEventListener('beforeunload', () => {
+    const formdata = {
+      formname: '',
+      formemail: '',
+      formtext: '',
     };
-
-    window.addEventListener('beforeunload', () => {
-      const formdata = {
-        formname: '',
-        formemail: '',
-        formtext: '',
-      };
-  
-      if ((!document.querySelector('#Name').value) || document.querySelector('#Name').value !== '') {
-        formdata.formname = document.querySelector('#Name').value;
-      }
-      if ((!document.querySelector('#email-bOX').value) || document.querySelector('#email').value !== '') {
-        formdata.formemail = document.querySelector('#emaiL-box').value;
-      }
-      if ((!document.querySelector('#form').value) || document.querySelector('#form').value !== '') {
-        formdata.formtext = document.querySelector('#textfield').value;
-      }
-  
-      JSON.stringify(formdata);
-      localStorage.setItem('formdata', JSON.stringify(formdata));
-    });
-
+    if ((!document.querySelector('#name').value) || document.querySelector('#name').value !== '') {
+      formdata.formname = document.querySelector('#name').value;
+    }
+    if ((!document.querySelector('#emailfield').value) || document.querySelector('#emailfield').value !== '') {
+      formdata.formemail = document.querySelector('#emailfield').value;
+    }
+    if ((!document.querySelector('#textfield').value) || document.querySelector('#textfield').value !== '') {
+      formdata.formtext = document.querySelector('#textfield').value;
+    }
+    JSON.stringify(formdata);
+    localStorage.setItem('formdata', JSON.stringify(formdata));
   });
 });
